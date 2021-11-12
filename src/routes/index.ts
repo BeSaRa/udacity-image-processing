91d71d691ab3ebf53728435c9f8a891f2f5188ed
BeSaRa <besara@windowslive.com>
2021-11-12 21:44:14 +0300

    chore: install/configure eslint and prettier to work well together

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,10 @@ import { Router, Response, Request } from 'express';
 import imageRoute from './api/image';
 
 const routes = Router();
-routes.get('/', (req: Request, res: Response) => {
-  res.send(200);
-});
-routes.use('/image', imageRoute);
 
+routes.get('/', (req: Request, res: Response) => {
+  res.send('API DONE!!');
+});
+
+routes.use('/image', imageRoute);
 export default routes;
